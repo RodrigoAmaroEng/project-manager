51a993d28538dd45b414d64498c8c339ee63bf56
Rodrigo <rodrigo@amaro.dev>
2021-08-10 10:10:23 -0500

    Modal window component

---
+++ src/components/modal.tsx
@@ -0,0 +1,12 @@
+import './modal.css';
+
+function ModalWindow(props: any) {
+  return (
+    <div className="modal-frame">
+      <div className="modal-window">
+        {props.children}
+      </div>
+    </div>
+  );
+}
+export default ModalWindow;
