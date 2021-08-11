221666e0f0f5002d63bcc53bd72038e7ee86260f
Rodrigo <rodrigo@amaro.dev>
2021-08-11 15:14:08 -0500

    Fixing issue with tab content not being updated

--- src/components/TabLayout.tsx
+++ src/components/TabLayout.tsx
@@ -2,39 +2,28 @@ import React, { useEffect, useState } from "react";
 import "./TabLayout.css";
 
 export function Tab(props: any) {
-
-  return (
-    <div className="tab" onClick={props.onClick} aria-selected={props.selected}>
-      {props.title}
-    </div>
-  );
+  return (<div className="tab-content">{props.children}</div>);
 }
 
 export function TabLayout(props: any) {
   const [selectedTab, setSelectedTab] = useState(0);
-  const [tabs, setTabs] = useState(
-    props.children.map((child: any, index: number) =>
-      React.cloneElement(child, {
-        onClick: () => {
-          setSelectedTab(index);
-        },
-        selected: index === selectedTab,
-      })
-    )
-  );
-
-  const updateChildren = () =>
-    tabs.map((child: any, index: number) =>
-      React.cloneElement(child, {
-        selected: index === selectedTab,
-      })
-    );
-  useEffect(() => setTabs(updateChildren()), [selectedTab]);
-
+  
   return (
     <div className="tab-layout">
-      <div className="tabs-placeholder">{tabs}</div>
-      <div className="tab-content">{tabs[selectedTab].props.children}</div>
+      <div className="tabs-placeholder">
+        {props.children.map((child: any, index: number) => {
+          return (
+            <div
+              className="tab"
+              onClick={() => setSelectedTab(index)}
+              aria-selected={index === selectedTab}
+            >
+              {child.props.title}
+            </div>
+          );
+        })}
+      </div>
+      {props.children[selectedTab]}
     </div>
   );
 }
